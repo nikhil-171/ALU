5807f9f2b12826ace38cbbf4b46035e29bdd21a4
Nikhil Mhatre <nikhilamhatre18@gmail.com>
2023-02-26 11:48:44 -0500

    Update ripple_adder_tb.vhd

--- ripple_adder_tb.vhd
+++ ripple_adder_tb.vhd
@@ -36,7 +36,19 @@ end ripple_adder_tb;
 
 architecture testbench of ripple_adder_tb is
 
-begin
+begin 
+    signal tb_a : in STD_LOGIC_vector (3 downto 0) := (others => '0');
+    signal tb_b : in STD_LOGIC_vector (3 downto 0) := (others => '0');
+    signal tb_cin : in std_logic_vector  (3 downto 0) := (others => '0');
+    signal  tb_y : out std_logic_vector (3 downto 0) := (others => '0');
+    signal tb_cout : out STD_LOGIC_vector (3 downto 0):= (others => '0'));
 
-
+component ripple_adder is
+port(
+a : in STD_LOGIC_vector (3 downto 0);
+           b : in STD_LOGIC_vector (3 downto 0);
+           cin : in std_logic_vector  (3 downto 0);
+           y : out std_logic_vector (3 downto 0);
+           cout : out STD_LOGIC_vector (3 downto 0));
+           
 end testbench;
